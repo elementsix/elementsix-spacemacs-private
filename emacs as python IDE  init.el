fbb7c600431df9235528f3213f834c0187a4ab17
elementsix <elementsixsoft@gmail.com>
2017-06-21 14:55:56 +0800

    Update emacs as python IDE  init.el

--- emacs as python IDE  init.el
+++ emacs as python IDE  init.el
@@ -5,6 +5,10 @@
 
 
 
+;;basic config
+;;-------------------------------------------------------------------------------------
+
+
 
 (when (>= emacs-major-version 24)
      (require 'package)
@@ -26,6 +30,9 @@
                 swiper
                 counsel
                 smartparens
+		elpy
+		flycheck
+		py-autopep8
                 ;; --- Major Mode ---
                 js2-mode
                 ;; --- Minor Mode ---
@@ -107,42 +114,12 @@
  )
 
 
-(require 'package)
-
-(add-to-list 'package-archives
-       '("melpa" . "http://melpa.org/packages/") t)
-
-(package-initialize)
-(when (not package-archive-contents)
-  (package-refresh-contents))
-
-(defvar myPackages
-  '(better-defaults
-    ein
-    elpy
-    flycheck
-    material-theme
-    py-autopep8))
-
-(mapc #'(lambda (package)
-    (unless (package-installed-p package)
-      (package-install package)))
-      myPackages)
-
-;; BASIC CUSTOMIZATION
-;; --------------------------------------
-
-(setq inhibit-startup-message t) ;; hide the startup message
-(load-theme 'material t) ;; load material theme
-(global-linum-mode t) ;; enable line numbers globally
-
 ;; PYTHON CONFIGURATION
 ;; --------------------------------------
 (setq python-shell-interpreter "python"
       python-shell-interpreter-args "-i")
 
 (elpy-enable)
-(elpy-use-ipython)
 
 ;; use flycheck not flymake with elpy
 (when (require 'flycheck nil t)
@@ -152,6 +129,3 @@
 ;; enable autopep8 formatting on save
 (require 'py-autopep8)
 (add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save)
-
-;; init.el ends here
-
